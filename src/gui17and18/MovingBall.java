dd85938dfd229b2c6cad9844067726192dd5b973
zero <zero_2009@hotmail.co.uk>
2014-11-11 22:50:27 +0000

    added speed options

--- src/gui17and18/MovingBall.java
+++ src/gui17and18/MovingBall.java
@@ -8,13 +8,23 @@ import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 public class MovingBall extends JFrame{
 	
+	private int speedOfBall = 500;
+	
 	private JButton button1;
 	private JPanel panel1;
+	
+	private JMenuBar jMenuBar;
+	private JMenu jMenu;
+	private JMenuItem[] jMenuItem;
 	
 	public MovingBall()
 	{
@@ -22,6 +32,7 @@ public class MovingBall extends JFrame{
 		setSize(500, 500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		intialiseData();
+		setupMenu();
 		layoutm();
 		actionssss();
 	}
@@ -30,6 +41,28 @@ public class MovingBall extends JFrame{
 	{
 		button1 = new JButton("start");
 		panel1 = new JPanel();
+		
+		jMenuBar = new JMenuBar();
+		jMenu = new JMenu("Menu");
+		jMenuItem = new JMenuItem[3];
+	}
+	
+	private void initialiseMenuItems()
+	{
+		jMenuItem[0] = new JMenuItem("fast");
+		jMenuItem[1] = new JMenuItem("medium");
+		jMenuItem[2] = new JMenuItem("slow");
+	}
+	
+	private void setupMenu()
+	{
+		this.initialiseMenuItems();
+		jMenu.add(jMenuItem[0]);
+		jMenu.add(jMenuItem[1]);
+		jMenu.add(jMenuItem[2]);
+		
+		jMenuBar.add(jMenu);
+		setJMenuBar(jMenuBar);
 	}
 	
 	private void layoutm()
@@ -42,13 +75,47 @@ public class MovingBall extends JFrame{
 	private void actionssss()
 	{
 		button1.addActionListener(new actionL());
+		
+		jMenuItem[0].addActionListener(new speedOfBall());
+		jMenuItem[1].addActionListener(new speedOfBall());
+		jMenuItem[2].addActionListener(new speedOfBall());
+	}
+
+	public int getSpeedOfBall() {
+		return speedOfBall;
+	}
+
+	public void setSpeedOfBall(int speedOfBall) {
+		this.speedOfBall = speedOfBall;
 	}
 
 	private class actionL implements ActionListener {
 
+		public void actionPerformed(ActionEvent e) {		
+				Runner tuni = new Runner();
+				tuni.start();
+		}
+	}
+	
+	private class speedOfBall implements ActionListener {
+
+		@Override
 		public void actionPerformed(ActionEvent e) {
-			Runner tuni = new Runner();
-			tuni.start();
+			switch(e.getActionCommand()) 
+			{
+			case "fast":
+				setSpeedOfBall(1);
+				break;
+			case "medium":
+				setSpeedOfBall(500);
+				break;
+			case "slow":
+				setSpeedOfBall(1000);
+				break;
+			default: 
+				System.out.println("Error");
+				break;
+			}
 		}
 	}
 	
@@ -73,7 +140,7 @@ public class MovingBall extends JFrame{
 				{
 					try
 					{
-						Thread.sleep(500);
+						Thread.sleep(getSpeedOfBall());
 					}
 					catch(InterruptedException e)
 					{
