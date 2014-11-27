06684d890fbebd6410da21845d4905b7322235cf
zero <zero_2009@hotmail.co.uk>
2014-11-27 22:05:59 +0000

    making collection demo

---
+++ src/collectionsCollector/DemoingTheCollection.java
@@ -0,0 +1,9 @@
+package collectionsCollector;
+
+public class DemoingTheCollection {
+
+	public static void main(String[] args) {
+		WhatisSet whatisSet = new WhatisSet();
+		whatisSet.activate();
+	}
+}
--- src/collectionsCollector/whatisSet.java
+++ src/collectionsCollector/whatisSet.java
@@ -1,5 +1,41 @@
 package collectionsCollector;
 
-public class whatisSet {
+import java.util.Scanner;
 
+public class WhatisSet {
+	
+	private Scanner scanner = new Scanner(System.in);
+	
+	public WhatisSet()
+	{
+		//
+	}
+	
+	public void activate()
+	{
+		menuWithSelectionOfSet(entryOfTypeOfValue());
+	}
+	
+	private String entryOfTypeOfValue()
+	{
+		System.out.println("Enter the type of 'Set' you want");
+		System.out.println("your options are: hashset, treeset or linkdedhashset");
+		String input = scanner.next();
+		return input;
+	}
+	
+	private void menuWithSelectionOfSet(String key)
+	{
+		switch (key.toLowerCase()) 
+		{
+		case "hashset":
+			System.out.println("I think it worked");
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	
 }
