7eb4ec465f497289a8059765536b543ad77fdce8
zero <zero_2009@hotmail.co.uk>
2015-09-10 19:16:30 +0100

    change

--- src/recur11and13/RecurringAT.java
+++ src/recur11and13/RecurringAT.java
@@ -1,5 +1,8 @@
 package recur11and13;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecurringAT implements PowerInterface {
 
 	public static void main(String[] args) 
@@ -8,6 +11,10 @@ public class RecurringAT implements PowerInterface {
 		RecurringAT oj = new RecurringAT();
 		oj.writevertical(1557);
 		System.out.println(oj.powerf(2, 3));
+		List<String> list = new ArrayList<String>();
+		addlist(list);
+		System.out.println(list);
+		combinationBreaker("oylel");
 	}
 
 	private void writevertical(int n)
@@ -41,6 +48,32 @@ public class RecurringAT implements PowerInterface {
 		else
 		{
 			return (1);
+		}
+	}
+	
+	public static void addlist(List<String> list)
+	{
+		if(list.size() < 5)
+		{
+			list.add("hello");
+			addlist(list);
+		}
+	}
+	
+	public static void combinationBreaker(String string)
+	{
+		int length = string.length();
+		
+		if(string.equals("yello"))
+		{
+			System.out.println("done");
+		}
+		else
+		{
+			for(int index = 0; index < string.length(); index++)
+			{
+				combinationBreaker(string.substring(0, index) + string.substring(index + 1, length));
+			}
 		}
 	}
 }
